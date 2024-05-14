f9fe686723e008b84d018546afc5d53c562c55e5
I- C <israrchattha0@gmail.com>
2024-05-14 12:31:52 +0500

    Initial commit: Adding EZ Texting project files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+
+    colors: {
+      color: {
+        1: '#34222E',
+      },
+      color: {
+        2: '#E2434B',
+      },
+
+      blacky: {
+        1: '#0C0C0C',
+      },
+    },
+  },
+  plugins: [],
+}
