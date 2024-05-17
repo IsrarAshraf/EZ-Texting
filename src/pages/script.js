8d80d85a58cf2e34561df748abcf88c7fbb35419
I- C <israrchattha0@gmail.com>
2024-05-17 21:16:06 +0500

    5/17/2024

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,5 +1,5 @@
 // scroll header
-  window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
     const header = document.getElementById('fixed-header');
     const scrollPosition = window.scrollY;
   
@@ -12,7 +12,33 @@
 // end
   
 
+// video_model
+const videoContainer = document.querySelector('.video-container');
+const modal = document.getElementById("videoModal");
+const modalVideo = document.getElementById("modalVideo");
+const closeBtn = document.querySelector(".close");
+const videoUrl = document.querySelector('.play-icon').href;
+
+videoContainer.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+    modalVideo.src = videoUrl;
+});
+
+closeBtn.addEventListener('click', function() {
+    modal.style.display = "none";
+    modalVideo.src = "";
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        modalVideo.src = "";
+    }
+});
+// video_model_end
   
+
 // testimonials 
 document.addEventListener('DOMContentLoaded', function() {
     const testimonials = document.querySelectorAll('.testimonial');
@@ -38,4 +64,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     showTestimonial(currentIndex);
 });
-{/* end */}
+/* end */
+
+// slider
+var swiper = new Swiper('.swiper-container', 
+{
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    loop: true,
+    freeMode: true,
+    autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    },
+    speed: 5000,
+});
+// slider_end
